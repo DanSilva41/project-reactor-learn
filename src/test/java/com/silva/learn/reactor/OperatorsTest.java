ebbaff7d6da9254de346886231ba35009242a669
Danilo da Silva Pereira <t0305dnl@prestadorcbmp.com.br>
2021-11-24 09:33:26 -0300

    SubscribeOn with frommCallable (separated thread)

--- src/test/java/com/silva/learn/reactor/OperatorsTest.java
+++ src/test/java/com/silva/learn/reactor/OperatorsTest.java
@@ -1,8 +1,15 @@
 package com.silva.learn.reactor;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
@@ -43,6 +50,102 @@ class OperatorsTest {
         StepVerifier.create(flux)
                 .then(flux::subscribe)
                 .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void multipleSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 1 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 2 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .then(flux::subscribe)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void multiplePublishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .then(flux::subscribe)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void publishAndSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .then(flux::subscribe)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void subscribeAndPublishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .subscribeOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 -> Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .then(flux::subscribe)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void subscribeOnIO() {
+        Mono<List<String>> monoLines = Mono.fromCallable(() ->
+                        Files.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("text-file.txt").toURI()), StandardCharsets.UTF_8))
+                .log()
+                .subscribeOn(Schedulers.boundedElastic());
+
+        StepVerifier.create(monoLines)
+                .expectSubscription()
+                .assertNext(list -> {
+                    assertFalse(list.isEmpty());
+                    log.info("Size list: {}", list.size());
+                })
                 .verifyComplete();
     }
 }
